84168569232fc282c8e1c15c6945804e5b3fbb9d
kris <shahikrishna2000@gmail.com>
2024-06-02 09:40:00 +0545

    first-cut FT

--- src/functional_tests/test_list_item_validation.py
+++ src/functional_tests/test_list_item_validation.py
@@ -7,15 +7,38 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Kris goes to the home page and accidentally tries to submit
         # an empty list item. He hits Enter on the empty input box
-
+        self.browser.get(self.live_server_url)
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
-
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element(By.CSS_SELECTOR, ".invalid-feedback").text,
+                "You can't have an empty list item",
+            )
+        )
+        
         # He tries again with some text for the item, which now works
-
+        self.browser.find_element(By.ID, "id_new_item").send_keys("Buy milk")
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1. Buy milk")
+    
         # Perversely, He now decides to submit a second blank list item
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
 
         # He receives a similar warning on the list page
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element(By.CSS_SELECTOR, ".invalid-feedback").text,
+                "You can't have an empty list item",
+            )
+        )
 
         # And he can correct it by filling some text in
-        self.fail("write me!")
+        self.browser.find_element(By.ID, "id_new_item").send_keys("Make tea")
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1. Buy milk")
+        self.wait_for_row_in_list_table("2. Make tea")
+
+        #Whenever you submit a form with Keys.ENTER or click something that is going to cause a page to load, you probably want an explicit wait for your next assertion.
