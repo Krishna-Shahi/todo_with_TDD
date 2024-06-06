b67357c6e62fb8d2f743988149faa3779967a963
kris <shahikrishna2000@gmail.com>
2024-06-06 20:29:08 +0545

    Advance forms, validations, unit tests and functional tests done

--- src/lists/views.py
+++ src/lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from lists.forms import ItemForm
+from lists.forms import ItemForm, ExistingListItemForm
 from lists.models import Item, List
 from django.core.exceptions import ValidationError
 
@@ -8,13 +8,14 @@ def home_page(request):
 
 def view_list(request, list_id):
     our_list = List.objects.get(id=list_id)
+    form = ExistingListItemForm(for_list=our_list)
     if request.method == "POST":
-        form = ItemForm(data=request.POST)
+        form = ExistingListItemForm(for_list=our_list, data=request.POST)
         if form.is_valid():
-            form.save(for_list=our_list)
+            form.save()
             return redirect(our_list)
     else:
-        form = ItemForm()
+        form = ExistingListItemForm(for_list=our_list)
     return render(request, "list.html", {"list": our_list, "form": form})
 
 def new_list(request):
