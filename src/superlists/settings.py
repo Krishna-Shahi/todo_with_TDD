6ad20a9ed07068ee80c8d147d7b82e45f497b325
kris <shahikrishna2000@gmail.com>
2024-06-20 13:39:51 +0545

    added a new app accounts and FT

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -47,7 +47,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -135,12 +138,18 @@ STATIC_ROOT = BASE_DIR / 'static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {"class": "logging.StreamHandler"},
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
     },
-    "loggers": {
-        "root": {"handlers": ["console"], "level": "INFO"},
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
     },
+    'root': {'level': 'INFO'},
 }
