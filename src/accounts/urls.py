f7b43e916abcacb275b9d1cd61ebb4cf4e435b7a
kris <shahikrishna2000@gmail.com>
2024-07-28 14:23:17 +0545

    Added logout and its test

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from accounts import views
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('send_login_email', views.send_login_email, name='send-login-email'),
     path('login', views.login, name='login'),
+    path('logout', auth_views.LogoutView.as_view(next_page="/"), name='logout'),
 ]
--- src/functional_tests/test_login.py
+++ src/functional_tests/test_login.py
@@ -44,3 +44,13 @@ class LoginTest(FunctionalTest):
             lambda: self.browser.find_element(By.LINK_TEXT, "Log out"))
         navbar = self.browser.find_element(By.CSS_SELECTOR,'.navbar')
         self.assertIn(TEST_EMAIL, navbar.text)
+
+        # Now he logs out
+        self.browser.find_element(By.LINK_TEXT, "Log out").click()
+
+        # He is logged out
+        self.wait_for(
+            lambda: self.browser.find_element(By.CSS_SELECTOR, "input[name=email]")
+        )
+        navbar = self.browser.find_element(By.CSS_SELECTOR, ".navbar")
+        self.assertNotIn(TEST_EMAIL, navbar.text)
